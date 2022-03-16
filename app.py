02c29a9e7fecbe76e1eec2d376863d14c155737f
Enes T <66560757+devenes@users.noreply.github.com>
2022-03-17 01:17:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,16 +18,30 @@ def convert(decimal_num):
 def main_post():
     if request.method == 'POST':
         alpha = request.form['number']
+
         if not alpha.isdecimal():
-            return render_template('index.html', developer_name='Enes', not_valid=True)
+            return render_template('index.html',
+                                   developer_name='Enes',
+                                   not_valid=True)
         number = int(alpha)
+
         if not 0 < number < 4000:
-            return render_template('index.html', developer_name='Enes', not_valid=True)
-        return render_template('result.html', number_decimal=number, number_roman=convert(number), developer_name='Serdar')
+            return render_template('index.html',
+                                   developer_name='Enes',
+                                   not_valid=True)
+
+        return render_template('result.html',
+                               number_decimal=number,
+                               number_roman=convert(number),
+                               developer_name='Enes')
+
     else:
-        return render_template('index.html', developer_name='Enes', not_valid=False)
+        return render_template('index.html',
+                               developer_name='Enes',
+                               not_valid=False)
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0',
+            port=80,
+            debug=False)
